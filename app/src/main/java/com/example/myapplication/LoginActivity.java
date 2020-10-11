5e64f895a0e2b783b68512c5405979a18d54d57b
Sage <1764575438@qq.com>
2020-10-11 23:04:52 +0800

    2020.10.11大修改，Login界面初步美化

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -6,7 +6,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.Code;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -24,6 +30,8 @@ public class LoginActivity extends Activity {
 
 
     Button but;
+    ImageView ima;
+    private String realCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +39,50 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
         LoginActivity.activityList.add(this);
 
-        but = findViewById(R.id.but);   //与布局文件的相关组建关联
-        but.setOnClickListener(new click());     //注册监听接口
+        initView();                     // 将所有对象与各布局文件的相关组件关联的步骤 以及 注册监听接口的步骤 集合到initView()方法内——称之为初始化、
+
+        //将验证码用图片的形式显示出来
+        ima.setImageBitmap(Code.getInstance().createBitmap());
+        realCode = Code.getInstance().getCode().toLowerCase();  //获取当前图片验证码的对应内容用于校验
+
+        ima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ima.setImageBitmap(Code.getInstance().createBitmap());
+                realCode = Code.getInstance().getCode();
+                Toast.makeText(LoginActivity.this, realCode, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    private void initView() {
+        // 初始化控件
+        but = findViewById(R.id.but);   //与布局文件的相关组件关联
+        ima = findViewById(R.id.iv_registeractivity_showCode);
+/*        mBtLoginactivityLogin = findViewById(R.id.bt_loginactivity_login);
+        mTvLoginactivityRegister = findViewById(R.id.tv_loginactivity_register);
+        mEtLoginactivityUsername = findViewById(R.id.et_loginactivity_username);
+        mEtLoginactivityPassword = findViewById(R.id.et_loginactivity_password);
+        mEtloginactivityPhonecodes = findViewById(R.id.et_loginactivity_phoneCodes);
+        mIvloginactivityShowcode = findViewById(R.id.iv_loginactivity_showCode);*/
+
+        // 设置点击事件监听器
+        but.setOnClickListener(new click());     //注册监听接口
+/*
+        mBtLoginactivityLogin.setOnClickListener(this);
+        mIvloginactivityShowcode.setOnClickListener(this);
+*/
+
+    }
+
 
     class click implements View.OnClickListener {   //定义一个类实现监听的接口
         public void onClick(View v) {
+            Animation animation=new AlphaAnimation(1.0f,0.0f);
+            animation.setDuration(300);
+            but.startAnimation(animation);
+
             Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
             // 创建好之后就可以通过它启动新的Activity
             startActivity(intent);
